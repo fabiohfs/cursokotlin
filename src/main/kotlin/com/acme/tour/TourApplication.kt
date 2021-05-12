c5c843a5fd62174e7135d29d529d1aa23f8c281d
fabiohfs <fabiohfs85@gmail.com>
2021-05-12 19:00:39 -0300

    Implementation of get method of Promocoes

--- src/main/kotlin/com/acme/tour/TourApplication.kt
+++ src/main/kotlin/com/acme/tour/TourApplication.kt
@@ -2,9 +2,23 @@ package com.acme.tour
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
+import com.acme.tour.model.Promocao
 
 @SpringBootApplication
-class TourApplication
+class TourApplication{
+	companion object{
+		val initialPromocoes = arrayOf(
+			Promocao(1, "Maravilhosa viagem a Cancun", "Cancun", true, 7, 4999.99),
+			Promocao(2, "Viagem radical com Rapel e Escalada", "Nova Zelândia", false, 12, 12000.0),
+			Promocao(3, "Viagem espiritual", "Tailândia", false, 17, 15000.0),
+			Promocao(4, "Viagem em família", "Gramado", false, 5, 3500.33)
+		)
+	}
+	@Bean
+	fun promocoes() = ConcurrentHashMap<Long,Promocao>(initialPromocoes.associateBy(Promocao::id))
+}
 
 fun main(args: Array<String>) {
 	runApplication<TourApplication>(*args)
--- src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
+++ src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
@@ -1,20 +1,23 @@
 package com.acme.tour.controller
 
 import com.acme.tour.model.Promocao
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class PromocaoController {
+    @Autowired
+    lateinit var promocoes: ConcurrentHashMap<Long,Promocao>
+
     @RequestMapping(value=["/sayHello"], method = arrayOf(RequestMethod.GET))
     fun sayHello(): String {
         return "Hello World"
     }
 
-    @RequestMapping(value=["/promocoes"], method = arrayOf(RequestMethod.GET))
-    fun getPromocao(): Promocao{
-        val promocao = Promocao(1, "Maravilhosa viagem a Cancun", "Cancun",true, 7, 4200.99)
-        return promocao
-    }
+    @RequestMapping(value=["/promocoes/{id}"], method = arrayOf(RequestMethod.GET))
+    fun getPromocao(@PathVariable id: Long) = promocoes[id]
 }
